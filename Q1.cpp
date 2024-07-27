b25915bcd8dcf9fd34e9e050db69036a931b9092
Hariji8004 <22103247@mail.jiit.ac.in>
2024-07-27 10:21:52 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sizen,av=0;
+    cout<<"Number of elements:";
+    cin>>sizen;
+    int array1[sizen];
+    for(int i=0;i<sizen;i++)
+    {
+        cin>>array1[i];
+        av+=array1[i];
 
+    }
+    cout<<"Average=";
+    cout<<av/sizen<<endl;
+
+}
